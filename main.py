3c4c69a37a365d7d19f3b0e9e5da4bc212195cff
alexkangzd <48787208+alexkangzd@users.noreply.github.com>
2021-07-08 16:18:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from flask_gravatar import Gravatar
 
 app = Flask(__name__)
 #TODO: Create a env variable if you want to upload online
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'YOUR KEY'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
